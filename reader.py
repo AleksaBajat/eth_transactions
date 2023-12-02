3404c95521711feecd4cc9ab196b362fd04a8232
AleksaBajat <aleksabajat2001@gmail.com>
2023-12-02 01:30:33 +0100

    presentation

--- reader.py
+++ reader.py
@@ -1,7 +1,172 @@
+import os
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, round as sround, avg as savg, max as smax, sum as ssum, unix_timestamp, hour, count, window
 
-spark = SparkSession.builder.master("local").appName("My App").getOrCreate()
-df = spark.read.parquet("part-00000-ae4c7c88-aeae-4100-9e25-08fc7f3ea308-c000.snappy.parquet")
-df = df.select("gas")
+class Columns:
+    DATE = "date"
+    HASH = "hash"
+    NONCE = "nonce"
+    TRANSACTION_INDEX = "transaction_index"
+    FROM_ADDRESS = "from_address"
+    TO_ADDRESS = "to_address"
+    VALUE = "value"
+    GAS_UNITS = "gas"
+    GAS_PRICE = "gas_price"
+    INPUT = "input"
+    TOTAL_GAS_USED = "receipt_cumulative_gas_used"
+    TRANSACTION_GAS_USED = "receipt_gas_used"
+    ADDRESS_CREATED = "receipt_contact_address"
+    TRANSACTION_STATUS = "receipt_status"
+    BLOCK_TIMESTAMP = "block_timestamp"
+    BLOCK_NUMBER = "block_number"
+    BLOCK_HASH = "block_hash"
+    TOTAL_FEE = "max_fee_per_gas"
+    PRIORITY_FEE = "max_priority_fee_per_gas"
+    TRANSACTION_TYPE = "transaction_type"
+    GAS_PRICE_EF = "receipt_effective_gas_price"
+    LAST_MODIFIED = "last_modified"
 
-df.show()
+
+sample_name = "part-00000-ae4c7c88-aeae-4100-9e25-08fc7f3ea308-c000.snappy.parquet"
+
+spark = SparkSession.builder.master("local").appName("Batch Processing").getOrCreate()
+df = spark.read.parquet(sample_name)
+
+print("Size of the data sample {}MB".format(os.path.getsize(sample_name)/1024/1024))
+print("Number of rows {}".format(df.count()))
+
+
+reference_df = (df.select(Columns.GAS_UNITS, Columns.GAS_PRICE, Columns.VALUE)
+          .withColumn("gas_price_gwei", col(Columns.GAS_PRICE)/1E9)
+          .withColumn("gas_price_eth", col(Columns.GAS_PRICE)/1E18)
+          .withColumn("gas_price_euro", col(Columns.GAS_PRICE)/1E18 * 1851)
+          .withColumn("gas_price_total_units_euro", sround(col("gas_price_euro")*col(Columns.GAS_UNITS), 2))
+          .withColumn("eth_transfered", col(Columns.VALUE)/1E18)
+          .withColumn("euro_transfered", col(Columns.VALUE)/1E18 * 1851))
+
+# AVERAGE PRICE OF GAS
+
+pog_df = (df.select(Columns.GAS_PRICE)
+          .withColumn("gas_price_gwei", col(Columns.GAS_PRICE)/1E9)
+          .withColumn("gas_price_eth", col(Columns.GAS_PRICE)/1E18)
+          .withColumn("gas_price_euro", col(Columns.GAS_PRICE)/1E18 * 1851)
+               )
+
+
+average_price = pog_df.agg(savg('gas_price_gwei')).collect()[0][0]
+
+print("Average price of gas in gwei : {}".format(average_price))
+
+
+# AVERAGE TRANSACTION PRICE
+
+tp_df = (df.select(Columns.GAS_PRICE, Columns.GAS_UNITS)
+          .withColumn("gas_price_gwei", col(Columns.GAS_PRICE)/1E9)
+          .withColumn("gas_price_eth", col(Columns.GAS_PRICE)/1E18)
+          .withColumn("gas_price_euro", col(Columns.GAS_PRICE)/1E18 * 1851)
+               )
+
+average_tp = tp_df.agg(savg(col(Columns.GAS_UNITS) * col("gas_price_euro"))).collect()[0][0]
+print("Average price of transaction in euro: {}".format(average_tp))
+
+# BIGGEST TRANSACTIONS
+
+bt_df = (df.select(Columns.GAS_PRICE, Columns.GAS_UNITS, Columns.VALUE)
+          .withColumn("gas_price_gwei", col(Columns.GAS_PRICE)/1E9)
+          .withColumn("gas_price_eth", col(Columns.GAS_PRICE)/1E18)
+          .withColumn("gas_price_euro", col(Columns.GAS_PRICE)/1E18 * 1851)
+          .withColumn("total_price_euro", col(Columns.GAS_UNITS) * col("gas_price_euro"))
+          .withColumn("value_euro", col(Columns.VALUE)/1E18 * 1851)
+               ).where(col(Columns.VALUE) != 0.0)
+bt_df_sorted = bt_df.orderBy(col("total_price_euro").desc())
+
+bt_df_sorted.show(10, truncate=False)
+
+# TOP ACCOUNTS BY TRANSACTION VOLUME
+
+ta_df = (df.select(Columns.FROM_ADDRESS)
+         .union(df.select(Columns.TO_ADDRESS))
+         .groupBy(Columns.FROM_ADDRESS)
+         .count()
+         .orderBy(col("count").desc()))
+
+ta_df.show(10, truncate=False)
+
+
+# FAILED TRANSACTIONS PERCENTAGE
+
+# ftp_df = (df.select(Columns.TRANSACTION_STATUS).where(Columns.TRANSACTION_STATUS == 0).count())
+ftp_df = (df.select(Columns.TRANSACTION_STATUS).groupBy(Columns().TRANSACTION_STATUS).count())
+
+results = ftp_df.collect()
+
+failed_count = results[0][1]
+success_count = results[1][1]
+
+ratio = failed_count / (success_count + failed_count)
+
+print("Failed transactions : {}%".format(round(ratio * 100, 4)))
+
+
+# AVERAGE GAS USED BY TRANSACTION
+
+agt_df = (df.select(Columns.GAS_UNITS).agg(savg(Columns().GAS_UNITS))).collect()[0][0]
+print("Average gas used up by transaction: {}".format(round(agt_df, 2)))
+
+
+# SMART CONTRACT INTERACTIONS VS TRANSFERS
+
+print("#########")
+contract_interactions_count = (df.select(Columns.INPUT).where(col(Columns.INPUT) != '0x').count())
+simple_transfers_count = (df.select(Columns.INPUT).where(col(Columns.INPUT) == '0x').count())
+
+percentage = round(contract_interactions_count/(simple_transfers_count + contract_interactions_count) * 100, 4)
+
+print("Smart contract interaction percentage: {}%".format(round(ratio * 100, 4)))
+print("Simple transfer percentage: {}%".format(round((100 - ratio) * 100, 4)))
+
+
+# BLOCKS REACHING GAS LIMIT
+
+GAS_LIMIT = 30000000  # Maximum size of the block
+
+block_utilization_df = df.groupBy("block_number") \
+    .agg(ssum(Columns.TRANSACTION_GAS_USED).alias("total_gas_used")) \
+    .withColumn("utilization", (col("total_gas_used") / GAS_LIMIT) * 100)
+
+spark_stream = SparkSession.builder.appName("Stream processing").getOrCreate()
+
+
+# HIGH VALUE TRANSACTION ALERTS
+
+threshold_value = 1000
+high_value_transactions = df_stream.filter(col("value") > threshold_value)
+high_utilization_blocks = block_utilization_df.filter(col("utilization") > 90)
+high_utilization_blocks.show()
+num_of_blocks = block_utilization_df.count()
+
+print("Number of blocks", num_of_blocks)
+print("Number of high_utilization_blocks (over 90%) {} - {}".format(high_utilization_blocks.count(), round(high_utilization_blocks.count()/num_of_blocks * 100), 2))
+
+
+# TRANSACTION SPEED VARIANCE BASED ON GAS PRICE
+
+transaction_speed_df = (df.withColumn("gas_price_gwei", col(Columns.GAS_PRICE)/1E9)
+            .withColumn("transaction_duration",
+                 unix_timestamp(Columns.LAST_MODIFIED) - unix_timestamp(Columns.BLOCK_TIMESTAMP)) \
+                      .groupBy("gas_price_gwei") \
+                          .agg(savg("transaction_duration").alias("average_duration")))
+
+transaction_speed_df.show()
+
+# MOST ACTIVE HOURS
+
+hourly_transaction_counts = (df.withColumn("hour", hour(Columns.BLOCK_TIMESTAMP))
+                                .groupBy("hour")
+                                .agg(count("*").alias("num_transactions"))
+                                .orderBy("num_transactions", ascending=False))
+
+hourly_transaction_counts.show()
+
+spark.stop()
+
